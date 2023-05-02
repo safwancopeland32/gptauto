f640fd332181e8b143245c76f990dbb29a27fd97
safwancopeland32 <127945494+safwancopeland32@users.noreply.github.com>
2023-05-02 15:47:01 +0200

    Initial commit

---
+++ apps/frontend/src/components/SettingsDrawer.tsx
@@ -0,0 +1,54 @@
+import { HamburgerIcon } from '@chakra-ui/icons';
+import { Image } from '@chakra-ui/react';
+import {
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  Heading,
+  IconButton,
+  useDisclosure,
+} from '@chakra-ui/react';
+import React from 'react';
+import { SettingsDrawerContent } from './SettingsDrawerContent';
+
+export function SettingsDrawer() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef<any>();
+
+  return (
+    <>
+      <IconButton
+        ref={btnRef}
+        aria-label='Open settings menu'
+        colorScheme='gray'
+        variant='ghost'
+        rounded='full'
+        size='lg'
+        icon={<HamburgerIcon />}
+        onClick={onOpen}
+      />
+      <Drawer
+        isOpen={isOpen}
+        placement='left'
+        size='md'
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>
+            <Image src="https://i.imgur.com/7aBUBOC.png" alt="Logo" />
+          </DrawerHeader>
+
+          <DrawerBody className='pretty-scrollbar'>
+            <SettingsDrawerContent />
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+}
